d00b654fc8c7863e156e3ddb5745f15cfd6823d0
varshamudududla <varshamudududla33@gmail.com>
2025-08-01 14:18:53 +0530

    Allow Netlify frontend in CORS settings

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,11 @@ const app = express();
 
 // === Middleware ===
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',
+  origin: ['http://127.0.0.1:5500', 'https://zingy-froyo-3ecbf5.netlify.app'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
